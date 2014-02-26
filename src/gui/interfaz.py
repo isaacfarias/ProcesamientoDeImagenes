e13d41eded55562192f63f694d580f617aab4fc1
isaax <men_one89@hotmail.com>
2014-02-25 23:39:37 -0600

    Inicio de la interfaz, porfin logre instalar las dependencias

--- src/gui/interfaz.py
+++ src/gui/interfaz.py
@@ -1,1 +1,24 @@
-#Este sera el codigo de la interfaz
+import gtk
+import pygtk
+
+
+pygtk.require('2.0')
+
+class VentanaPrincipal:
+
+   def delete_event(self, widget,event,data = None):
+      print "Cerrando..."
+      return false
+      
+   def destroy(self,widget,data = None):
+      gtk.main_quit()
+   
+   def __init__(self):
+      self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+      self.window.connect("delete_event", self.delete_event)
+      self.window.connect("destroy", self.destroy)
+      self.window.show()
+      gtk.main()
+      
+if __name__ == "__main__":
+   ventana = VentanaPrincipal()
