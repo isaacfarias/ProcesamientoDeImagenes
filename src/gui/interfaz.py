3305420ab41474a66c1564371ae9434e37135c29
isaax <men_one89@hotmail.com>
2014-02-27 08:25:24 -0600

    Boton de apertura de archivo

--- src/gui/interfaz.py
+++ src/gui/interfaz.py
@@ -7,16 +7,35 @@ pygtk.require('2.0')
 class VentanaPrincipal:
 
    def delete_event(self, widget,event,data = None):
-      print "Cerrando..."
-      return false
+      print "Bye..."
+      return False
       
    def destroy(self,widget,data = None):
       gtk.main_quit()
+      
+   def selectFile(self,widget,data):
+      return None
    
    def __init__(self):
       self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
       self.window.connect("delete_event", self.delete_event)
       self.window.connect("destroy", self.destroy)
+      self.window.set_title("Vision x computadoras")
+      self.window.set_default_size(640,480)
+      
+      self.hbox = gtk.HBox(False, 0)
+      self.vbox = gtk.VBox(False, 0)
+      
+      self.hbox.pack_start(self.vbox,True,True,0)
+      
+      self.fileChooser = gtk.FileChooserButton('Selecciona un archivo')
+      self.file = None
+      
+      self.vbox.pack_start(self.fileChooser,True,True, 0)
+      self.vbox.show()
+      self.hbox.show()
+      self.fileChooser.show()
+      self.window.add(self.hbox)
       self.window.show()
       gtk.main()
       
